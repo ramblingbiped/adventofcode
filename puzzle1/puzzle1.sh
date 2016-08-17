a2c91392f99dfdc150fdb6da39847b6f35434b29
Will Nave <will.nave@gmail.com>
2016-08-17 11:16:41 -0500

    changes to structure

--- puzzle1/puzzle1.sh
+++ puzzle1/puzzle1.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-INPUT=/home/wanave/scripts/Bash/adventofcode/puzzle1.input
-
+INPUT=puzzle1.input
 
 counter=0
 
 while read -n1 char; do
-    
     if [[ $char == "(" ]]; then
         let counter=counter+1
     elif [[ $char == ")" ]]; then
         let counter=counter-1
     fi
-
-    echo $counter
-
 done < $INPUT
 
 echo $counter
